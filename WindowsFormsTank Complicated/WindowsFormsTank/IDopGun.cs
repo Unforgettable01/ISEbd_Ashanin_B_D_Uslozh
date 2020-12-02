91901906a89b7093da740e44d44e9c4aabe937f1
Boris <boryanich01@gmail.com>
2020-12-02 21:56:17 +0400

    lab_2_Only_IDopGun

--- WindowsFormsTank Complicated/WindowsFormsTank/IDopGun.cs
+++ WindowsFormsTank Complicated/WindowsFormsTank/IDopGun.cs
@@ -3,11 +3,11 @@
 namespace WindowsFormsTank
 {
     interface IDopGun {
-        //int SetNumberDopGun
-        //{
-        //    set;
-        //}
-        //void DrawDopGun(Graphics g, Color dopColor, float _startPosX, float _startPosY) ; //метод для отриисовки дополнительного орудия
+        int SetNumberDopGun
+        {
+            set;
+        }
+        void DrawDopGun(Graphics g, Color dopColor, float _startPosX, float _startPosY); //метод для отриисовки дополнительного орудия
 
     }
 }
