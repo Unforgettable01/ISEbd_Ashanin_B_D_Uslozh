62e3607a4a0864010f80d0b47b90cc0bafc59c4f
Boris <boryanich01@gmail.com>
2020-12-03 11:26:40 +0400

    Lab_1_U_ready

--- WindowsFormsTank Complicated/WindowsFormsTank/DopGuns.cs
+++ WindowsFormsTank Complicated/WindowsFormsTank/DopGuns.cs
@@ -6,7 +6,7 @@ namespace WindowsFormsTank
     class DopGuns
     {
         //закрытое поле
-        private  Enumeration _numberDopGun;
+        private Enumeration _numberDopGun;
 
         public int SetNumberDopGun
         {
@@ -18,6 +18,7 @@ namespace WindowsFormsTank
                         _numberDopGun = Enumeration.One;
                         break;
                     case 2:
+
                         _numberDopGun = Enumeration.Two;
                         break;
                     case 3:
@@ -27,36 +28,59 @@ namespace WindowsFormsTank
             }
         }
 
-      
+
         public void DrawDopGun(Graphics g, Color dopColor, float _startPosX, float _startPosY)
         {
-            
-            
-            Pen pen = new Pen(Color.Black);
-
 
             if (_numberDopGun == Enumeration.One)
             {
-                Brush mainColorGun = new SolidBrush(dopColor);
-                g.DrawRectangle(pen, _startPosX - 20, _startPosY + 20, 95, 10);              //большое орудие
-                g.FillRectangle(mainColorGun, _startPosX - 20, _startPosY + 20, 95, 10);    //большое орудие
-                return;
+                DrawDopGun_1(g, dopColor, _startPosX, _startPosY);
+
             }
 
             if (_numberDopGun == Enumeration.Two)
             {
-                Pen penSmallGun = new Pen(Color.Black, 5);
-                g.DrawLine(penSmallGun, _startPosX + 165, _startPosY + 15, _startPosX + 190, _startPosY + 15);// малое орудие в бр.машине №1
+                
+                DrawDopGun_2(g, dopColor, _startPosX, _startPosY);
+
             }
 
             if (_numberDopGun == Enumeration.Three)
             {
-                Pen penGun = new Pen(Color.Black, 3);
-                g.DrawLine(penGun, _startPosX + 225, _startPosY + 35, _startPosX + 280, _startPosY + 35);// малое орудие в бр.машине №2 горизонтальная часть
-                g.DrawLine(penGun, _startPosX + 225, _startPosY + 35, _startPosX + 225, _startPosY + 50);// малое орудие в бр.машине №2 вертикальная часть
+                
+                DrawDopGun_3(g, dopColor, _startPosX, _startPosY);
             }
+        }
 
-           
+        public void DrawDopGun_1(Graphics g, Color dopColor, float _startPosX, float _startPosY)
+        {
+
+            Pen pen = new Pen(Color.Black);
+
+            Brush mainColorGun = new SolidBrush(dopColor);
+            g.DrawRectangle(pen, _startPosX - 20, _startPosY + 20, 95, 10);              //большое орудие
+            g.FillRectangle(mainColorGun, _startPosX - 20, _startPosY + 20, 95, 10);    //большое орудие
+
+
+        }
+
+        public void DrawDopGun_2(Graphics g, Color dopColor, float _startPosX, float _startPosY)
+        {
+            DrawDopGun_1(g, dopColor, _startPosX, _startPosY);
+            Pen penSmallGun = new Pen(Color.Black, 5);
+            g.DrawLine(penSmallGun, _startPosX + 165, _startPosY + 15, _startPosX + 190, _startPosY + 15);// малое орудие в бр.машине №1
+
+
+        }
+
+        public void DrawDopGun_3(Graphics g, Color dopColor, float _startPosX, float _startPosY)
+        {
+            DrawDopGun_2(g, dopColor, _startPosX, _startPosY);
+            Pen penGun = new Pen(Color.Black, 3);
+            g.DrawLine(penGun, _startPosX + 225, _startPosY + 35, _startPosX + 280, _startPosY + 35);// малое орудие в бр.машине №2 горизонтальная часть
+            g.DrawLine(penGun, _startPosX + 225, _startPosY + 35, _startPosX + 225, _startPosY + 50);// малое орудие в бр.машине №2 вертикальная часть
+
+
         }
     }
 
